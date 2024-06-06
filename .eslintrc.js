ebca7d881122a6a2557c493319a96d1379636069
alagunoff <lagunov.artamon@yandex.ru>
2024-06-06 12:12:10 +0300

    fix: fix eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,43 +1,47 @@
 module.exports = {
   root: true,
   settings: {
-    'import/resolver': {
+    "import/resolver": {
       typescript: true,
       node: true,
     },
     react: {
-      version: 'detect',
+      version: "detect",
     },
   },
-  plugins: ['@typescript-eslint'],
-  parser: '@typescript-eslint/parser',
+  plugins: ["@typescript-eslint"],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
     tsconfigRootDir: __dirname,
   },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended-type-checked',
-    'plugin:@typescript-eslint/stylistic-type-checked',
-    'plugin:import/recommended',
-    'plugin:import/typescript',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-    'plugin:@next/next/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:@typescript-eslint/stylistic-type-checked",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "plugin:@next/next/recommended",
   ],
   rules: {
-    'import/order': [
-      'error',
+    "import/order": [
+      "error",
       {
-        groups: [['builtin', 'external'], ['internal'], ['parent', 'sibling', 'index']],
-        'newlines-between': 'always',
+        groups: [
+          ["builtin", "external"],
+          ["internal"],
+          ["parent", "sibling", "index"],
+        ],
+        "newlines-between": "always",
         alphabetize: {
-          order: 'asc',
+          order: "asc",
           caseInsensitive: true,
         },
       },
     ],
   },
-  ignorePatterns: ['**/*.js', '**/*.mjs'],
+  ignorePatterns: [".next/**", "*.js"],
 };
