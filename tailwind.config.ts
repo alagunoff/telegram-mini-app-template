2409156ff2c91b60fcae20f6e7a7074875d540d4
alagunoff <lagunov.artamon@yandex.ru>
2024-06-07 17:33:45 +0300

    refactor: refine tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,15 +2,6 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: ["./app/**/*.{ts,tsx}"],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
 };
 
 export default config;
