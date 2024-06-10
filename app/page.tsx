eff294a9b3d1d10db62bf4195a1db779382b5729
alagunoff <lagunov.artamon@yandex.ru>
2024-06-10 15:22:41 +0300

    chore: remove log

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import {
   useUtils,
 } from "@tma.js/sdk-react";
 import { useTonWallet, TonConnectButton } from "@tonconnect/ui-react";
+import classNames from "classnames";
 import Image from "next/image";
 
 function Page() {
@@ -15,7 +16,7 @@ function Page() {
   const themeParams = useThemeParams(true);
   const utils = useUtils(true);
   const wallet = useTonWallet();
-  console.log(initData);
+
   return (
     <>
       <h1 className="mb-2 text-2xl font-bold">Template for mini apps</h1>
@@ -113,26 +114,14 @@ function Page() {
         </ul>
       </section>
       <section>
-        <div className="mb-3 flex items-center gap-x-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width={20}
-            height={20}
-            fill="none"
-            viewBox="0 0 56 56"
-          >
-            <path
-              fill="#0098ea"
-              d="M28 56c15.464 0 28-12.536 28-28S43.464 0 28 0 0 12.536 0 28s12.536 28 28 28Z"
-            />
-            <path
-              fill="#fff"
-              d="M37.56 15.628H18.44c-3.516 0-5.745 3.792-3.976 6.858l11.801 20.455c.77 1.335 2.7 1.335 3.47 0l11.804-20.455c1.767-3.06-.462-6.858-3.975-6.858h-.003ZM26.255 36.807l-2.57-4.974-6.202-11.092c-.409-.71.096-1.62.953-1.62h7.816V36.81l.003-.002ZM38.51 20.739l-6.2 11.096-2.57 4.972V19.119h7.817c.857 0 1.362.91.953 1.62Z"
-            />
-          </svg>
+        <div
+          className={classNames("flex items-center justify-between gap-x-2", {
+            "mb-5": !!wallet,
+          })}
+        >
           <h2 className="text-xl font-semibold">Wallet connection</h2>
+          <TonConnectButton />
         </div>
-        <TonConnectButton className="mb-5" />
         {wallet && (
           <>
             {"imageUrl" in wallet && (
